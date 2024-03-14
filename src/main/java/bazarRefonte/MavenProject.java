a159fe45164e4036aa8ad38be97277ad9c0ba123
Pascal Poizat <pascal.poizat@gmail.com>
2024-03-14 19:58:06 +0100

    clean up

--- src/main/java/bazarRefonte/MavenProject.java
+++ src/main/java/bazarRefonte/MavenProject.java
@@ -1,10 +1,15 @@
 package bazarRefonte;
 
 import java.nio.file.Path;
+import java.util.Set;
+import java.util.HashSet;
+import org.apache.maven.model.Dependency;
 
 public class MavenProject implements Project {
+
+    Set<Dependency> dependencies = new HashSet<>();
+
     @Override
     public void dump(Path path) {
-
     }
 }
