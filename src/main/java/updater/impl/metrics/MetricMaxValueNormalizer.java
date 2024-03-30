22ce4174692851ab2f1e8213c594380152bcf90a
DaJaime <jaime.damien@gmail.com>
2024-03-30 16:33:14 +0100

    Metric normalizer: add interface

--- src/main/java/updater/impl/metrics/MetricMaxValueNormalizer.java
+++ src/main/java/updater/impl/metrics/MetricMaxValueNormalizer.java
@@ -10,17 +10,9 @@ import java.util.Map;
 import java.util.Optional;
 
 public class MetricNormalizer {
-    private static MetricNormalizer instance;
     private final Map<MetricType, Double> mapMetricMax = new HashMap<>();
 
     private MetricNormalizer(){}
-
-    public static synchronized MetricNormalizer getInstance() {
-        if (instance == null) {
-            instance = new MetricNormalizer();
-        }
-        return instance;
-    }
 
     public static void normalize(UpdateGraph<UpdateNode, UpdateEdge> graph){
         Map<MetricType, Double> mapMetricMax = new HashMap<>();
@@ -39,17 +31,5 @@ public class MetricNormalizer {
                 score.ifPresent(aDouble -> release.put(metricType, max == 0 ? 0 : (aDouble / max)));
             }
         }
-    }
-
-    public void put(MetricType metricType, double value) {
-        Double currentValue = mapMetricMax.get(metricType);
-        if (currentValue == null || value > currentValue) {
-            mapMetricMax.put(metricType, value);
-        }
-    }
-
-    public double normalize(MetricType type, double score) {
-        double max = mapMetricMax.get(type);
-        return max == 0 ? 0 : (score/max);
     }
 }
